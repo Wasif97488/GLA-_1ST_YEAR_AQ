41617bf8e71a0d9722e916f8e7eda6825cc35cbe
Wasif97488 <w4wasifrock@gmail.com>
2026-01-20 18:07:14 +0530

    Datatypes, Operators, User Input

---
+++ Lec01_08_jan/Wasif.java
@@ -0,0 +1,6 @@
+class Wasif
+{
+public static void main(String args[])
+{
+System.out.println("Hello , i am Wasif");
+}
---
+++ Lec09_20_Jan/Flow_Control.java
@@ -0,0 +1,35 @@
+//Flow Control:
+
+// if-else , switch case , for loop , while loop , do-while loop , //for-each loop (break , continue ,default )
+
+
+class Flow_Control
+{
+public static void main(String args[])
+{
+int a=10;
+char c = 'b';
+if(a>30)
+{
+System.out.println("Good Morning");
+}
+else
+{
+System.out.println("Good AfterNoon");
+}
+//System.out.println("Good Night");
+//system.out.println("dheh");
+int b=20;
+if(b>30)
+
+System.out.println("Good Morning");
+
+else
+
+System.out.println("Good AfterNoon");
+
+}
+}
+
+
+
